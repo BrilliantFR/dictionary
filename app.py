eae55f22861778dc3633c90fa4e7013b82774bbf
Brilliant <briliantramadhani@gmail.com>
2023-11-14 03:02:28 +0700

    First Commit

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
+from os.path import join, dirname
+from dotenv import load_dotenv
+
 from flask import Flask, request, render_template, redirect, url_for, jsonify
 from pymongo import MongoClient
 import requests
 from datetime import datetime
 from bson import ObjectId
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
 
 app = Flask(__name__)
 
